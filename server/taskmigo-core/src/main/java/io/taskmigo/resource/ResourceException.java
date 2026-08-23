f9442f0d6e47268ec4f9aecb4dabfa034801bdd2
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-23 18:18:45 +0700

    Annotate the serialization identifier

--- server/taskmigo-core/src/main/java/io/taskmigo/resource/ResourceException.java
+++ server/taskmigo-core/src/main/java/io/taskmigo/resource/ResourceException.java
@@ -1,7 +1,10 @@
 package io.taskmigo.resource;
 
+import java.io.Serial;
+
 public final class ResourceException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public enum Type {
