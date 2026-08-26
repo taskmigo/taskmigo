421356d736a54cd948599f79d9000cb864cb00d5
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-26 10:21:28 +0700

    Share domain failure infrastructure

--- server/modules/organization/src/main/java/io/taskmigo/access/AccessException.java
+++ server/modules/organization/src/main/java/io/taskmigo/access/AccessException.java
@@ -1,25 +1,16 @@
 package io.taskmigo.access;
 
-import java.io.Serial;
+import io.taskmigo.foundation.domain.DomainException;
+import io.taskmigo.foundation.domain.DomainFailureType;
 
 /// Reports an access-domain failure with a transport-neutral category.
-public final class AccessException extends RuntimeException {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
+public final class AccessException extends DomainException {
 
     public enum Type {
         BAD_REQUEST,
     }
 
-    private final Type type;
-
     AccessException(Type type, String message) {
-        super(message);
-        this.type = type;
-    }
-
-    public Type type() {
-        return this.type;
+        super(DomainFailureType.valueOf(type.name()), message);
     }
 }
