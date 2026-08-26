55c1cc1d62e88bd5c730908dcd4611f9393cc1f9
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-26 10:20:59 +0700

    Use foundation domain contract

--- server/modules/organization/build.gradle.kts
+++ server/modules/organization/build.gradle.kts
@@ -7,6 +7,7 @@ description = "Organization, users, groups, roles, and permissions"
 dependencies {
     api(libs.jspecify)
     api(libs.spring.modulith.starter.core)
+    api(project(":modules:foundation"))
 
     implementation(libs.spring.boot.starter.data.jpa)
 }
