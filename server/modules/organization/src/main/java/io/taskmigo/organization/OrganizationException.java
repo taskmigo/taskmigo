3c4919e7fcd0539cc3c5fb82da263ede37790a07
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-26 10:21:15 +0700

    Share domain failure infrastructure

--- server/modules/organization/src/main/java/io/taskmigo/organization/OrganizationException.java
+++ server/modules/organization/src/main/java/io/taskmigo/organization/OrganizationException.java
@@ -1,12 +1,10 @@
 package io.taskmigo.organization;
 
-import java.io.Serial;
+import io.taskmigo.foundation.domain.DomainException;
+import io.taskmigo.foundation.domain.DomainFailureType;
 
 /// Reports an organization-domain failure with a transport-neutral category.
-public final class OrganizationException extends RuntimeException {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
+public final class OrganizationException extends DomainException {
 
     public enum Type {
         BAD_REQUEST,
@@ -14,19 +12,11 @@ public final class OrganizationException extends RuntimeException {
         CONFLICT,
     }
 
-    private final Type type;
-
     OrganizationException(Type type, String message) {
-        super(message);
-        this.type = type;
+        super(DomainFailureType.valueOf(type.name()), message);
     }
 
     OrganizationException(Type type, String message, Throwable cause) {
-        super(message, cause);
-        this.type = type;
-    }
-
-    public Type type() {
-        return this.type;
+        super(DomainFailureType.valueOf(type.name()), message, cause);
     }
 }
