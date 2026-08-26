c72b27c52b61ac3d68a94cdeefba239b41f8e4d1
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-26 10:21:04 +0700

    Use foundation pagination primitives

--- server/modules/project/build.gradle.kts
+++ server/modules/project/build.gradle.kts
@@ -7,6 +7,7 @@ description = "Projects, memberships, authorization, and project history"
 dependencies {
     api(libs.jspecify)
     api(libs.spring.modulith.starter.core)
+    api(project(":modules:foundation"))
     api(project(":modules:organization"))
 
     implementation(libs.jackson.databind)
