e5a7c9dc0854a5a018a8c11090dcf71cda6110e8
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-27 20:08:06 +0700

    Disallow deprecated Buffer construction

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import unicorn from "eslint-plugin-unicorn";
 
 export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: { unicorn },
+    rules: {
+      "unicorn/no-new-buffer": "error",
+    },
+  },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
