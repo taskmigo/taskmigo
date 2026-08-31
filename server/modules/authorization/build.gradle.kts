4775691093413ee0380b53e4b2f44d5be71048e3
Phong <58473133+phongphongg@users.noreply.github.com>
2026-08-31 16:49:40 +0700

    Persist custom ACL policies

--- server/modules/authorization/build.gradle.kts
+++ server/modules/authorization/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     api(libs.jspecify)
     api(libs.spring.modulith.starter.core)
     implementation(libs.spring.boot.core.starter)
+    implementation(libs.spring.boot.starter.data.jpa)
 
     testImplementation(libs.spring.boot.starter.test)
 }
