f5393e1f0fcdefdf46cea4935e5e2e67d617f5e0
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-13 19:21:50 +0700

    Use Spring Boot 4 EntityScan package

--- server/modules/identity/src/test/java/io/taskmigo/identity/persistence/query/JpaObjectAuthorizationPredicateBinderIntegrationTest.java
+++ server/modules/identity/src/test/java/io/taskmigo/identity/persistence/query/JpaObjectAuthorizationPredicateBinderIntegrationTest.java
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.jpa.domain.Specification;
