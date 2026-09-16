95d4b93ffcaa98bec4f233e16469c52134fcd1cf
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-16 23:12:10 +0700

    Remove raw generic mocks from Statement activation test

--- server/modules/authorization/src/test/java/io/taskmigo/authorization/statement/StatementActivationResultTypeTest.java
+++ server/modules/authorization/src/test/java/io/taskmigo/authorization/statement/StatementActivationResultTypeTest.java
@@ -5,14 +5,17 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.taskmigo.authorization.object.ObjectAuthorization;
+import io.taskmigo.authorization.object.ObjectAuthorizationPredicate;
 import io.taskmigo.authorization.persistence.query.ObjectAuthorizationPredicateBinder;
 import io.taskmigo.authorization.persistence.query.QueryPredicateBinder;
 import io.taskmigo.authorization.persistence.statement.StatementEntity;
 import io.taskmigo.authorization.persistence.statement.StatementRepository;
 import io.taskmigo.language.LanguageCompiler;
+import io.taskmigo.query.QueryPredicate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
+import org.springframework.data.jpa.domain.Specification;
 
 class StatementActivationResultTypeTest {
 
@@ -24,8 +27,8 @@ class StatementActivationResultTypeTest {
         StatementService service = new StatementService(
             repository,
             new StatementPolicyValidator(mock(ObjectAuthorization.class), new LanguageCompiler()),
-            mock(QueryPredicateBinder.class),
-            mock(ObjectAuthorizationPredicateBinder.class)
+            new QueryBinderStub(),
+            new ObjectBinderStub()
         );
 
         service.create(
@@ -39,5 +42,43 @@ class StatementActivationResultTypeTest {
         );
 
         verify(repository).save(ArgumentMatchers.any(StatementEntity.class));
+    }
+
+    private static final class QueryBinderStub implements QueryPredicateBinder<StatementInfo, StatementEntity> {
+
+        @Override
+        public Class<StatementInfo> queryType() {
+            return StatementInfo.class;
+        }
+
+        @Override
+        public Class<StatementEntity> domainType() {
+            return StatementEntity.class;
+        }
+
+        @Override
+        public Specification<StatementEntity> bind(QueryPredicate<StatementInfo> predicate) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private static final class ObjectBinderStub
+        implements ObjectAuthorizationPredicateBinder<StatementInfo, StatementEntity>
+    {
+
+        @Override
+        public Class<StatementInfo> objectType() {
+            return StatementInfo.class;
+        }
+
+        @Override
+        public Class<StatementEntity> domainType() {
+            return StatementEntity.class;
+        }
+
+        @Override
+        public Specification<StatementEntity> bind(ObjectAuthorizationPredicate<StatementInfo> predicate) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
