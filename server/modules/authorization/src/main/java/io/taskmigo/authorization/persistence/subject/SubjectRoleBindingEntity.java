394c73d95f59f046e4982b0b0a665feb3b8daea9
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-16 22:58:00 +0700

    Remove CanBeFinal suppression from Role bindings

--- server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/subject/SubjectRoleBindingEntity.java
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/subject/SubjectRoleBindingEntity.java
@@ -13,7 +13,7 @@ import java.util.UUID;
     name = "subject_role_bindings",
     uniqueConstraints = @UniqueConstraint(columnNames = { "subject_type", "subject_id", "role_id" })
 )
-@SuppressWarnings({ "CanBeFinal", "NotNullFieldNotInitialized" })
+@SuppressWarnings("NotNullFieldNotInitialized")
 public class SubjectRoleBindingEntity {
 
     @Id
