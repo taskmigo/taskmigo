b35341a39caf2a6e14adf2bfeaf7b75730234e35
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-17 20:05:00 +0700

    Suppress Qodana nullability false positive on Query binder contract

--- server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/QueryPredicateBinder.java
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/QueryPredicateBinder.java
@@ -1,10 +1,10 @@
 package io.taskmigo.authorization.persistence.query;
 
 import io.taskmigo.query.QueryPredicate;
-import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.domain.Specification;
 
 /// Binds one Access Control-owned Query Predicate to its persistence model.
+@SuppressWarnings("NullableProblems")
 public interface QueryPredicateBinder<Q, E> {
     /// Returns the logical query contract accepted by this binder.
     Class<Q> queryType();
@@ -13,5 +13,5 @@ public interface QueryPredicateBinder<Q, E> {
     Class<E> domainType();
 
     /// Converts a trusted logical predicate into a database-side specification.
-    Specification<E> bind(@NonNull QueryPredicate<Q> predicate);
+    Specification<E> bind(QueryPredicate<Q> predicate);
 }
