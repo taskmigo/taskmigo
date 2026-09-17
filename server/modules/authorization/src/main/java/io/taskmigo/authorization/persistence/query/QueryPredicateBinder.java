e17197bd08028e64e00316f0831ab9bbecf76254
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-17 19:48:37 +0700

    Make Query Predicate binder nullness explicit

--- server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/QueryPredicateBinder.java
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/QueryPredicateBinder.java
@@ -1,6 +1,7 @@
 package io.taskmigo.authorization.persistence.query;
 
 import io.taskmigo.query.QueryPredicate;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.domain.Specification;
 
 /// Binds one Access Control-owned Query Predicate to its persistence model.
@@ -12,5 +13,5 @@ public interface QueryPredicateBinder<Q, E> {
     Class<E> domainType();
 
     /// Converts a trusted logical predicate into a database-side specification.
-    Specification<E> bind(QueryPredicate<Q> predicate);
+    Specification<E> bind(@NonNull QueryPredicate<Q> predicate);
 }
