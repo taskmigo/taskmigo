7e0a79ee11f125a8999280f2f4238f53d756df39
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-17 19:56:31 +0700

    Make Identity Object Authorization binder nullness explicit

--- server/modules/identity/src/main/java/io/taskmigo/identity/persistence/query/ObjectAuthorizationPredicateBinder.java
+++ server/modules/identity/src/main/java/io/taskmigo/identity/persistence/query/ObjectAuthorizationPredicateBinder.java
@@ -1,6 +1,7 @@
 package io.taskmigo.identity.persistence.query;
 
 import io.taskmigo.authorization.object.ObjectAuthorizationPredicate;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.domain.Specification;
 
 /// Binds one resource-owned Object Authorization predicate to its persistence model.
@@ -12,5 +13,5 @@ public interface ObjectAuthorizationPredicateBinder<Q, E> {
     Class<E> domainType();
 
     /// Converts a trusted logical predicate into a database-side specification.
-    Specification<E> bind(ObjectAuthorizationPredicate<Q> predicate);
+    Specification<E> bind(@NonNull ObjectAuthorizationPredicate<Q> predicate);
 }
