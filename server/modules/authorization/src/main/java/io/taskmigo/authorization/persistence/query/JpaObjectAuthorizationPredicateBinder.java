b4f1f67097877afbc47976f89cebb21b4462e963
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-17 19:49:05 +0700

    Preserve Object Authorization binder nullness contract

--- server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/JpaObjectAuthorizationPredicateBinder.java
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/persistence/query/JpaObjectAuthorizationPredicateBinder.java
@@ -3,6 +3,7 @@ package io.taskmigo.authorization.persistence.query;
 import io.taskmigo.authorization.object.ObjectAuthorizationPredicate;
 import io.taskmigo.authorization.object.persistence.ObjectAuthorizationPredicateModel;
 import java.util.Map;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.domain.Specification;
 
 /// Creates an Access Control-owned Object Authorization predicate binder for an entity mapping.
@@ -37,7 +38,7 @@ public final class JpaObjectAuthorizationPredicateBinder<Q, E> implements Object
     }
 
     @Override
-    public Specification<E> bind(ObjectAuthorizationPredicate<Q> predicate) {
+    public Specification<E> bind(@NonNull ObjectAuthorizationPredicate<Q> predicate) {
         if (!(predicate instanceof ObjectAuthorizationPredicateModel model)) {
             throw new IllegalArgumentException("unsupported Object Authorization Predicate implementation");
         }
