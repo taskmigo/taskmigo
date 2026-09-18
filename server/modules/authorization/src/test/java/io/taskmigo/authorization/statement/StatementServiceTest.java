8e9c729ea530a90cabd417f6d6ba2cad6a0ebedb
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-18 21:57:53 +0700

    fix: compose Statement service test with JPA adapter

--- server/modules/authorization/src/test/java/io/taskmigo/authorization/statement/StatementServiceTest.java
+++ server/modules/authorization/src/test/java/io/taskmigo/authorization/statement/StatementServiceTest.java
@@ -11,8 +11,8 @@ import io.taskmigo.authorization.core.AuthorizationException;
 import io.taskmigo.authorization.object.ObjectAuthorization;
 import io.taskmigo.authorization.persistence.query.ObjectAuthorizationPredicateBinder;
 import io.taskmigo.authorization.persistence.query.QueryPredicateBinder;
+import io.taskmigo.authorization.persistence.statement.JpaStatementOperations;
 import io.taskmigo.authorization.statement.internal.DefaultStatementService;
-import io.taskmigo.authorization.statement.internal.StatementStore;
 import io.taskmigo.authorization.persistence.statement.StatementEntity;
 import io.taskmigo.authorization.persistence.statement.StatementRepository;
 import io.taskmigo.language.CompiledSource;
@@ -45,7 +45,7 @@ class StatementServiceTest {
         ObjectAuthorizationPredicateBinder<StatementInfo, StatementEntity> objectBinder = mock(
             ObjectAuthorizationPredicateBinder.class
         );
-        this.service = new DefaultStatementService((StatementStore) this.statements, policyValidator);
+        this.service = new DefaultStatementService(new JpaStatementOperations(this.statements, queryBinder, objectBinder), policyValidator);
     }
 
     @Test
