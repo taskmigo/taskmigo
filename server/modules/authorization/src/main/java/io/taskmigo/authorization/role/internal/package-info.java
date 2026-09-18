80295f41b17e94dc741a9cabfc6fa4a5f1fac9f9
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-18 20:57:05 +0700

    Mark Role application package null-safe

---
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/role/internal/package-info.java
@@ -0,0 +1,6 @@
+/// Contains Role application services and their persistence-neutral ports.
+@NullMarked
+package io.taskmigo.authorization.role.internal;
+
+import org.jspecify.annotations.NullMarked;
+
