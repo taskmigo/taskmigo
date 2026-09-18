e814d2f52675189d96384e0beb27d971b1b0e6dc
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-18 09:45:37 +0700

    Define internal Role application layer

---
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/role/internal/package-info.java
@@ -0,0 +1,5 @@
+/// Contains Access Control Role application services and persistence ports.
+@NullMarked
+package io.taskmigo.authorization.role.internal;
+
+import org.jspecify.annotations.NullMarked;
