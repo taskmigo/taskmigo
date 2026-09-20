86cc7feada8a9fccf9c6f477ec03d8bba8d77e49
Phong <58473133+phongphongg@users.noreply.github.com>
2026-09-20 15:23:23 +0700

    Apply remaining repository formatting

--- server/modules/authorization/src/main/java/io/taskmigo/authorization/provisioning/internal/DefaultAuthorizationProvisioningService.java
+++ server/modules/authorization/src/main/java/io/taskmigo/authorization/provisioning/internal/DefaultAuthorizationProvisioningService.java
@@ -88,7 +88,10 @@ class DefaultAuthorizationProvisioningService implements AuthorizationProvisioni
         if (mutation.created()) {
             this.roleHierarchies.synchronize(this.roleHierarchies.loadForMutation());
         }
-        return new AuthorizationProvisioningResult<>(mutation.id(), provisioningChange(mutation.created(), mutation.changed()));
+        return new AuthorizationProvisioningResult<>(
+            mutation.id(),
+            provisioningChange(mutation.created(), mutation.changed())
+        );
     }
 
     @Override
